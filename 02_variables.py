6bb2e189ad03e9dd08cbbd699291215e0ff612a5
narayanj1193 <101072172+narayanj1193@users.noreply.github.com>
2022-03-11 14:35:36 +1300

    Add files via upload

--- 02_variables.py
+++ 02_variables.py
@@ -1,8 +1,15 @@
+# code to learn about variables
+
+# variables can be a string
 greeting = "hello world"
 
+# they can also be numbers
 fav_num = 7
 print(fav_num * 2)
 
 num_string = "5"
 print(num_string * 5)
+
+# Below is a constant (the name is in UPPERCASE)
+PI = 3.14
 
--- 03_getting_input.py
+++ 03_getting_input.py
@@ -1,12 +1,21 @@
+# Ask the user for their name
 username = input("What is your name?")
 
+# ask user for their favourite number
 fav_num = int(input("Favourite Number?"))
 
+# double, halve, and square the number
 double = fav_num * 2 
 half = fav_num / 2 
 squared = fav_num * fav_num
 
+# Empty print helps code look good
+print()
+
+# Greet user
 print("Hi {}, your favourite number is {}.". format(username, fav_num, ))
+
+# Tell the user their favourite number doubled, halved, and squared
 print("Your favourite number doubled is {}". format(double))
 print("Your favourite number halved is {}". format(half))
 print("Your favourite number squared is {}". format(squared))
